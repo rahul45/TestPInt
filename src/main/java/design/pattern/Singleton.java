45c05dce3e32338b7467631db17c8f429194ff19
Bhadouria, Rahul Singh <rahul.bhadouria@optum.com>
2021-06-04 16:12:36 +0530

    added

---
+++ src/main/java/design/pattern/Singleton.java
@@ -0,0 +1,17 @@
+package design.pattern;
+
+public class Singleton {
+    private static Singleton INSTANCE= null;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+
+}
